c8840140ac5989bac255aea01ca1bf80fe4e2fc8
Siyalov <91375207+Siyalov@users.noreply.github.com>
2022-07-12 13:38:46 +0300

    modal_opened

--- modal.js
+++ modal.js
@@ -1,9 +1,14 @@
-const btn = document.querySelector(".main_buttons button"); 
-
+const btns = document.querySelectorAll(".main_buttons button"); 
 const modal = document.getElementById('modal');
-
-btn.addEventListener('click', function() {
-
+const close = document.getElementById('close_btn');
 
 
+btns.forEach(btn => {
+   btn.addEventListener('click', function() {
+      modal.classList.add('modal_opened')        
+   })
+})
+
+close.addEventListener('click', function() {
+   modal.classList.remove('modal_opened') 
 })
